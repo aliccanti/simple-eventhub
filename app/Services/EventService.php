93179b1d242871548c8c7535dd5d144eb2126bf7
Alice Cavalcanti <alicecavalcanti27@gmail.com>
2025-10-21 16:38:12 -0300

    feat: event and user services

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\DTO\EventInputDto;
+use App\Exceptions\ParticipantUserCannotCreateEventException;
 use App\Models\Event;
 use App\Repositories\Interfaces\EventRepositoryInterface;
 use App\Repositories\Interfaces\UserRepositoryInterface;
@@ -10,16 +11,14 @@ use App\Repositories\Interfaces\UserRepositoryInterface;
 class EventService
 {
     public function __construct(protected EventRepositoryInterface $repository,
-                                protected UserRepositoryInterface $userRepository)
-    {
-    }
+        protected UserRepositoryInterface $userRepository) {}
 
     public function store(EventInputDto $dto): Event
     {
         $organizer = $this->userRepository->getById($dto->organizerId);
 
-        if (!$organizer->isOrganizer()) {
-            throw new \Exception('Somente organizadores podem criar eventos.');
+        if (! $organizer->isOrganizer()) {
+            throw new ParticipantUserCannotCreateEventException();
         }
 
         return $this->repository->create($dto->title, $dto->description, $dto->date, $dto->ticketPrice, $dto->capacity, $organizer->id);
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,14 +4,12 @@ namespace App\Services;
 
 use App\DTO\UserInputDto;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
-    public function __construct(protected UserRepositoryInterface $repository)
-    {
-    }
+    public function __construct(protected UserRepositoryInterface $repository) {}
 
     public function create(UserInputDto $dto): User
     {
