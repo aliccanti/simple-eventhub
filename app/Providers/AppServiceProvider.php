134b1f6dc31253b7729162c282a59c85a55f1e00
Alice Cavalcanti <alicecavalcanti27@gmail.com>
2025-10-21 16:40:08 -0300

    feat: bind services in providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
+use App\Repositories\EventRepository;
+use App\Repositories\Interfaces\EventRepositoryInterface;
+use App\Repositories\Interfaces\PurchaseRepositoryInterface;
 use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Repositories\PurchaseRepository;
 use App\Repositories\UserRepository;
+use App\Services\AuthorizerService;
+use App\Services\Interfaces\AuthorizerServiceInterface;
+use App\Services\Interfaces\NotificationServiceInterface;
+use App\Services\NotificationService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(EventRepositoryInterface::class, EventRepository::class);
+        $this->app->bind(PurchaseRepositoryInterface::class, PurchaseRepository::class);
+        $this->app->bind(AuthorizerServiceInterface::class, AuthorizerService::class);
+        $this->app->bind(NotificationServiceInterface::class, NotificationService::class);
     }
 
     /**
