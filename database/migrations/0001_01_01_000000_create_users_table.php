139356286b7021cc2e82e4f1d8d4021be17c000e
Alice Cavalcanti <alicecavalcanti27@gmail.com>
2025-10-21 16:44:11 -0300

    feat: migration for cache and update users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('type', ['organizer','participant'])->default('participant');
+            $table->enum('type', ['organizer', 'participant'])->default('participant');
             $table->timestamps();
         });
     }
